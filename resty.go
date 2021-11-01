51def3fc1dc25c1ec5c7ef2ff5f5969409b2c7ab
storezhang <storezhang@gmail.com>
2021-11-01 17:03:58 +0800

    代码迁移到pangum组织下

--- resty.go
+++ resty.go
@@ -3,19 +3,20 @@ package http
 import (
 	`crypto/tls`
 
+	`github.com/go-resty/resty/v2`
 	`github.com/pangum/pangu`
 	`github.com/storezhang/gox`
 )
 
 func newClient(config *pangu.Config) (client *Client, err error) {
-	panguConfig := new(panguConfig)
-	if err = config.Load(panguConfig); nil != err {
+	_panguConfig := new(panguConfig)
+	if err = config.Load(_panguConfig); nil != err {
 		return
 	}
-	clientConfig := panguConfig.Http.Client
+	clientConfig := _panguConfig.Http.Client
 
 	restyClient := resty.New()
-	if "" != clientConfig.Proxy.Host {
+	if `` != clientConfig.Proxy.Host {
 		restyClient.SetProxy(clientConfig.Proxy.Addr())
 	}
 	if 0 != clientConfig.Timeout {
@@ -28,14 +29,14 @@ func newClient(config *pangu.Config) (client *Client, err error) {
 		// nolint:gosec
 		restyClient.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
 	} else {
-		if "" != clientConfig.Certificate.Root {
+		if `` != clientConfig.Certificate.Root {
 			restyClient.SetRootCertificate(clientConfig.Certificate.Root)
 		}
 		if 0 != len(clientConfig.Certificate.Clients) {
 			certificates := make([]tls.Certificate, 0, len(clientConfig.Certificate.Clients))
 			for _, c := range clientConfig.Certificate.Clients {
-				certificate, err := tls.LoadX509KeyPair(c.Public, c.Private)
-				if nil != err {
+				certificate, certificateErr := tls.LoadX509KeyPair(c.Public, c.Private)
+				if nil != certificateErr {
 					continue
 				}
 				certificates = append(certificates, certificate)
@@ -55,13 +56,13 @@ func newClient(config *pangu.Config) (client *Client, err error) {
 	if 0 != len(clientConfig.Cookies) {
 		restyClient.SetCookies(clientConfig.Cookies)
 	}
-	if "" != clientConfig.Auth.Type {
+	if `` != clientConfig.Auth.Type {
 		switch clientConfig.Auth.Type {
 		case gox.AuthTypeBasic:
 			restyClient.SetBasicAuth(clientConfig.Auth.Username, clientConfig.Auth.Password)
 		case gox.AuthTypeToken:
 			restyClient.SetAuthToken(clientConfig.Auth.Token)
-			if "" != clientConfig.Auth.Scheme {
+			if `` != clientConfig.Auth.Scheme {
 				restyClient.SetAuthScheme(clientConfig.Auth.Scheme)
 			}
 		}
