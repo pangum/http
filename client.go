a50367f27186d8ded101eca8d41b08418f500999
storezhang <storezhang@gmail.com>
2021-11-01 13:47:37 +0800

    代码迁移到pangum组织下

--- client.go
+++ client.go
@@ -2,9 +2,24 @@ package http
 
 import (
 	`github.com/go-resty/resty/v2`
+	`github.com/storezhang/gox`
+	`github.com/storezhang/gox/field`
 )
 
 // Client 客户端封装
 type Client struct {
 	*resty.Client
+}
+
+func (c *Client) ResponseFields(rsp *resty.Response) (fields gox.Fields) {
+	if nil == rsp {
+		return
+	}
+
+	fields = []gox.Field{
+		field.Int(`code`, rsp.StatusCode()),
+		field.String(`body`, string(rsp.Body())),
+	}
+
+	return
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
-module github.com/storezhang/pangu-http
+module github.com/pangum/http
 
 go 1.16
 
 require (
 	github.com/go-resty/resty/v2 v2.6.0
-	github.com/storezhang/gox v1.5.0
+	github.com/storezhang/gox v1.7.9
 	github.com/storezhang/pangu v1.2.4
 )
 
