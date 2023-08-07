3216e6a39aeb1ca91d2b65f4cbd2a2d5dc1bedc1
storezhang <storezhang@gmail.com>
2023-08-07 10:03:19 +0800

    fix(配置): 默认不需要明确配置

--- internal/core/wrapper.go
+++ internal/core/wrapper.go
@@ -4,5 +4,5 @@ type Wrapper struct {
 	Http struct {
 		// 客户端配置
 		Client Config `json:"client" yaml:"client" xml:"client" toml:"client" validate:"required"`
-	} `json:"http" yaml:"http" xml:"http" toml:"http" validate:"required"`
+	} `json:"http" yaml:"http" xml:"http" toml:"http"`
 }
